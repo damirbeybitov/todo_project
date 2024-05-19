2cee74d317244d4c232a5726936e18c5a11f83c4
damirbeybitov <damirbey7@gmail.com>
2024-05-20 02:30:59 +0500

    add dockers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -52,34 +52,4 @@ func main() {
 
 	service := service.NewService(handler)
 	service.LaunchServer()
-
-	// Вызов метода RegisterUser
-	// registerResponse, err := microServiceClients.UserClient.RegisterUser(context.Background(), &pbUser.RegisterUserRequest{
-	// 	Username: "damir",
-	// 	Email:    "damir@example.com",
-	// 	Password: "password",
-	// })
-	// if err != nil {
-	// 	log.Fatalf("could not register user: %v", err)
-	// }
-	// log.Printf("Registered user with ID: %s", registerResponse.Id)
-
-	// Вызов метода GetUserProfile
-	// getUserProfileResponse, err := microserviceClients.UserClient.GetUserProfile(context.Background(), &pbUser.GetUserProfileRequest{
-	// 	Id: "1", // Предполагается, что это ID только что зарегистрированного пользователя
-	// })
-	// if err != nil {
-	// 	log.Fatalf("could not get user profile: %v", err)
-	// }
-	// log.Printf("Got user profile: %+v", getUserProfileResponse.User)
-
-	// Вызов метода DeleteUser
-	// deleteUserResponse, err := microserviceClients.UserClient.DeleteUser(context.Background(), &pbUser.DeleteUserRequest{
-	// 	Username: "damir",
-	// 	Password: "password",
-	// })
-	// if err != nil {
-	// 	log.Fatalf("could not delete user: %v", err)
-	// }
-	// log.Printf(deleteUserResponse.Message)
 }
